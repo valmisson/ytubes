de2c7d5d481044c097bf652011dcb558924732d5
Valmisson Grizorte <missongrizorte@gmail.com>
2021-10-15 18:32:49 -0300

    chore: update user-agent

--- src/constants/default.ts
+++ src/constants/default.ts
@@ -3,7 +3,7 @@ import { SearchTypes } from '../types/shims'
 
 export const headers = {
   'Access-Control-Allow-Origin': '*',
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
+  'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:92.0) Gecko/20100101 Firefox/92.0'
 }
 
 export const searchTypes: SearchTypes = {
